b9f6dbeb5cb02e038e1b86b8a87bea853e2069d2
Pierre Plourde <pierre@spartancsharp.net>
2024-02-08 11:06:16 -0500

    Create Code
    Recreate the guest book project without looking back. Just take the concept and create the application.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -4,12 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using GuestBookLibrary.Models;
+
 namespace ConsoleUI
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
+			List<GuestModel> guestlist = new List<GuestModel>();
+
+			GetGuestInformation(guestlist);
+
+			PrintOutGuestList(guestlist);
+
+			_ = Console.ReadLine();
+		}
+
+		private static void PrintOutGuestList(List<GuestModel> guests)
+		{
+			foreach ( GuestModel guest in guests )
+			{
+				Console.WriteLine(guest.GuestMessage);
+			}
+		}
+
+		private static void GetGuestInformation(List<GuestModel> guests)
+		{
+			string moreguestcoming;
+
+			do
+			{
+				bool isagevalid;
+				GuestModel guest = new GuestModel
+				{
+					FirstName = GetInfoFromConsole("What is your first name: "),
+					LastName = GetInfoFromConsole("What is your last name: ")
+				};
+
+				do
+				{
+					string agetext = GetInfoFromConsole("How old are you: ");
+					isagevalid = int.TryParse(agetext, out int agenumber);
+					if ( !isagevalid )
+					{
+						Console.WriteLine("Please enter your age as a number.");
+					}
+
+					guest.Age = agenumber;
+				} while ( !isagevalid );
+
+				guest.Hometown = GetInfoFromConsole("What is your hometowm: ");
+				guest.MessageToHost = GetInfoFromConsole("What is your message for the host: ");
+
+				guests.Add(guest);
+
+				moreguestcoming = GetInfoFromConsole("Are more guest coming (yes/no): ");
+
+				Console.Clear();
+
+			} while ( moreguestcoming.ToLower() == "yes" );
+		}
+
+		private static string GetInfoFromConsole(string message)
+		{
+			string output;
+
+			Console.Write(message);
+			output = Console.ReadLine();
+
+			return output;
 		}
 	}
 }
---
+++ GuestBookLibrary/Models/GuestModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBookLibrary.Models
+{
+	public class GuestModel
+	{
+		public string FirstName
+		{
+			get; set;
+		}
+		public string LastName
+		{
+			get; set;
+		}
+		public int Age
+		{
+			get; set;
+		}
+		public string Hometown
+		{
+			get; set;
+		}
+		public string MessageToHost
+		{
+			get; set;
+		}
+		public string GuestMessage
+		{
+			get
+			{
+				return $"{FirstName} {LastName}, {Age}, from {Hometown}: {MessageToHost}";
+			}
+		}
+	}
+}
